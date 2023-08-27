b4ea482bfaa212a15c1e900fe72e4a89f67aeffb
kumarsatwik <38569860+Kumarsatwik@users.noreply.github.com>
2023-08-27 18:59:33 +0530

    Update Frontend.jsx

--- src/Components/skills/Frontend.jsx
+++ src/Components/skills/Frontend.jsx
@@ -36,14 +36,14 @@ const Frontend = () => {
             </div>
 
             <div className="skills__group">
-                {/* <div className="skills__data">
+                <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
                     
                     <div>
                         <h3 className="skills__name">Bootstrap</h3>
                         <span className="skills__level">Intermediate</span>
                     </div>  
-                </div> */}
+                </div>
 
                 <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
@@ -63,14 +63,7 @@ const Frontend = () => {
                     </div>  
                 </div>
 
-                <div className="skills__data">
-                    <i className='bx bx-badge-check'></i>
-                    
-                    <div>
-                        <h3 className="skills__name">Next.js</h3>
-                        <span className="skills__level">Basic</span>
-                    </div>  
-                </div>
+                
 
                 {/* <div className="skills__data">
                     <i className='bx bx-badge-check'></i>
